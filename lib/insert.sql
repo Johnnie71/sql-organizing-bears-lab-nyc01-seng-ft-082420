542dfd5a631d9557947177ca15b1ed3ff8c0560a
johnnie71 <johnnie.agonz71@gmail.com>
2020-09-02 11:22:12 -0400

    methods complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Choclate", 12, "M", "brown", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 4, "M", "white", "goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 8, "F", "black", "calm", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 15, "M", "brown", "calm", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 2, "F", "black", "goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 10, "M", "white", "moody", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 18, "F", "chocolate", "goofy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (NULL, 12, "M", "white", "moody", 0);
